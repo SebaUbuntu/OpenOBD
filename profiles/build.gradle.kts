9bfc0d31e4ac0f7ba0c3fc37241007edfc8db2e5
Sebastiano Barezzi <seba@sebaubuntu.dev>
2026-03-20 14:51:28 +0100

    OpenOBD: `androidLibrary` -> `android`
    Change-Id: I537a2e4a51bb21a3df90d2e8bdafb8991d5b4734

--- profiles/build.gradle.kts
+++ profiles/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 kotlin {
-    androidLibrary {
+    android {
         compileSdk = libs.versions.android.compileSdk.get().toInt()
         minSdk = libs.versions.android.minSdk.get().toInt()
         namespace = "dev.sebaubuntu.openobd.profiles"
