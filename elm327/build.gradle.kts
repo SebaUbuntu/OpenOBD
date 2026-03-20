e566b575331a933f37f7e107ee5f047d64dbcb43
Sebastiano Barezzi <seba@sebaubuntu.dev>
2026-03-20 14:40:34 +0100

    OpenOBD: Fix ELM327 module namespace
    Change-Id: I196276ee019e15747e7ee04e49153f886fc768e5

--- elm327/build.gradle.kts
+++ elm327/build.gradle.kts
@@ -12,7 +12,7 @@ kotlin {
     androidLibrary {
         compileSdk = libs.versions.android.compileSdk.get().toInt()
         minSdk = libs.versions.android.minSdk.get().toInt()
-        namespace = "dev.sebaubuntu.openobd.commands.elm327"
+        namespace = "dev.sebaubuntu.openobd.elm327"
     }
 
     listOf(
