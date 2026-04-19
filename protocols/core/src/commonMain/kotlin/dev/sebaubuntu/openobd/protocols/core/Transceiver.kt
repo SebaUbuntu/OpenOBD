a17f6634477eb6f5b01df4d2cf4fed199f63d634
Sebastiano Barezzi <seba@sebaubuntu.dev>
2026-04-19 04:30:31 +0200

    OpenOBD: protocols: Fork transceiver caps into different interfaces
    Change-Id: Iab91b17248f7b33e6686fe9d67efd2dc733dc5da

--- protocols/core/src/commonMain/kotlin/dev/sebaubuntu/openobd/protocols/core/Transceiver.kt
+++ protocols/core/src/commonMain/kotlin/dev/sebaubuntu/openobd/protocols/core/Transceiver.kt
@@ -5,24 +5,7 @@
 
 package dev.sebaubuntu.openobd.protocols.core
 
-import kotlinx.coroutines.flow.Flow
-
 /**
  * A protocol transceiver.
  */
-interface Transceiver<F : Frame> {
-    /**
-     * Flow that emits newly received frames.
-     *
-     * Once a downstream starts observing, the flow will only emit frames received after the
-     * subscription.
-     */
-    fun receive(): Flow<F>
-
-    /**
-     * Send a frame.
-     *
-     * @param frame The frame to send
-     */
-    suspend fun transmit(frame: F)
-}
+interface Transceiver<F : Frame> : Receiver<F>, Transmitter<F>
---
+++ protocols/core/src/commonMain/kotlin/dev/sebaubuntu/openobd/protocols/core/Transmitter.kt
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: Sebastiano Barezzi
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package dev.sebaubuntu.openobd.protocols.core
+
+/**
+ * A protocol transmitter.
+ */
+interface Transmitter<F : Frame> {
+    /**
+     * Send a frame.
+     *
+     * @param frame The frame to send
+     */
+    suspend fun transmit(frame: F)
+}
